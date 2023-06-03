55a6140fc60eee55f6b0fa8e134076af8fd30773
Mauricio Rojas Barrientos <maalroja@bancolombia.com.co>
2023-06-02 23:36:03 -0400

    chore: add main metadata service

--- metadata/cmd/main.go
+++ metadata/cmd/main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"log"
+	"net/http"
+
+	"github.com/mrojasb2000/movieapp/metadata/internal/controller/metadata"
+	httphandler "github.com/mrojasb2000/movieapp/metadata/internal/handler/http"
+	"github.com/mrojasb2000/movieapp/metadata/internal/repository/memory"
 )
 
 func main() {
 	log.Println("Starting the movie metadata service")
-
+	repo := memory.New()
+	ctrl := metadata.New(repo)
+	h := httphandler.New(ctrl)
+	http.Handle("/metadata", http.HandlerFunc(h.GetMetadata))
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		panic(err)
+	}
 }
